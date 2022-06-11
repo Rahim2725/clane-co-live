e17ae20c0419a50c0719f6acdd1a7b356e9d16a1
Rahim2725 <info.abdurrahim12@gmail.com>
2022-06-12 00:56:50 +0600

    deploy this projec

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,14 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Services = () => {
+    const [service, setService] = useState();
+    useEffect(() => {
+        axios()
+    }, [])
+
     return (
         <div>
             <h1>this services</h1>
